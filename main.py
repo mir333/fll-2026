123e2b3ede552e39318264f7c7482be40f93cd48
KaSO <111751774+kaso-0@users.noreply.github.com>
2025-06-13 15:50:39 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+import orientation
+from hub import motion_sensor
+from hub import port
+import runloop
+import motor_pair
+import motor
+motor_pair.pair(motor_pair.PAIR_1, port.A, port.B)
+
+kp = -5
+async def goforward(vzdalenost, speed):
+    motion_sensor.reset_yaw(0)
+    pozice = 0
+    motor.reset_relative_position(port.A, 0)
+    motor.reset_relative_position(port.B, 0)
+    await runloop.sleep_ms(50)
+    while vzdalenost > pozice * -1:
+        motorcorrection = (motion_sensor.tilt_angles()[0] * kp)
+        pozice = motor.relative_position(port.A) * + motor.relative_position(port.B) /720 * 17.6
+        motor.set_duty_cycle(port.A, speed + motorcorrection)
+        motor.set_duty_cycle(port.B, (speed - motorcorrection ) * -1)
+        print(motorcorrection)
+        await runloop.sleep_ms(100)
+    motor.stop(port.A)
+    motor.stop(port.B)
+
+async def turnRight(angle, speed):
+    motion_sensor.reset_yaw(0)
+    while motion_sensor.tilt_angles()[0] >= angle * -10:
+        motor.set_duty_cycle(port.A, speed)
+        print(motion_sensor.tilt_angles()[0])
+        await runloop.sleep_ms(100)
+    motor.stop(port.A)
+
+async def turnLeft(angle, speed):
+    motion_sensor.reset_yaw(0)
+    while motion_sensor.tilt_angles()[0] <= angle * 10:
+        motor.set_duty_cycle(port.B, speed)
+        print(motion_sensor.tilt_angles()[0])
+        await runloop.sleep_ms(100)
+    motor.stop(port.B)
+
+async def main():
+    #await goforward(100000, 10000)
+    await turnRight(90, 4000)
+    motion_sensor.reset_yaw(0)
+    print(motor.relative_position(port.A) + motor.relative_position(port.B))
+    while False:
+        print(motion_sensor.tilt_angles()[0])
+        await runloop.sleep_ms(200)
+runloop.run(main())
